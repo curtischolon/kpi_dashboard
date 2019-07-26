10bab35f3c20d074a3f25dac8b1862fb8ed6d10a
curtis.cholon@gmail.com <u80lbYFay7Su>
2019-07-26 15:49:29 -0700

    added charts

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,20 +1,135 @@
+// Stores Serviced Y/N
 google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart1);
 
-function drawChart() {
+function drawChart1() {
     var data = google.visualization.arrayToDataTable([
-        ['Category', 'Serviced Y', 'Serviced N', {role: 'annotation'}],
+        ['Category', 'N', 'Y', {role: 'annotation'}],
         ['Atlanta', 10, 90, ''],
         ['San Antonio', 20, 80, ''],
+        ['Jackson', 30, 70, ''],
+        ['Fife', 40, 60, ''],
+        ['Ontario', 30, 70, ''],
+        ['Salt Lake', 25, 75, ''],
+        ['Hawaii', 0, 100, ''],
+        ['Alaska', 5, 95, ''],
+        ['ALL', 20, 80, '']
     ]);
 
     var options = {
-        width: 600,
-        height: 400,
-        legent: {postion: 'top', maxlines: 3},
-        bar: {groupWidth: '75%'},
-        isStacked: true
+        title: 'Stores Serviced by Agency Y/N (%)',
+        legend: {position: 'top', maxlines: 3},
+        bar: {groupWidth: '50%'},
+        isStacked: 'percent',
+        vAxis: {
+            minValue: 0,
+            ticks: [0, .25, 0.5, 0.75, 1]
+        },
+        height: '100%'
     };
     var chart = new google.visualization.ColumnChart(document.getElementById("serviced"));
     chart.draw(data, options);
+}
+
+
+// Serviced at Required Time
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart2);
+
+function drawChart2() {
+    var data = google.visualization.arrayToDataTable([
+        ['Category', 'Under', 'Ok', 'Over', {role: 'annotation'}],
+        ['Atlanta', 10, 80, 10, ''],
+        ['San Antonio', 20, 70, 10, ''],
+        ['Jackson', 10, 70, 20, ''],
+        ['Fife', 40, 50, 10, ''],
+        ['Ontario', 30, 50, 20, ''],
+        ['Salt Lake', 25, 70, 5, ''],
+        ['Hawaii', 0, 100, 0, ''],
+        ['Alaska', 5, 95, 0, ''],
+        ['ALL', 20, 70, 10, '']
+    ]);
+
+    var options = {
+        title: 'Stores Serviced On Time (%)',
+        legend: {position: 'top', maxlines: 3},
+        bar: {groupWidth: '50%'},
+        isStacked: 'percent',
+        vAxis: {
+            minValue: 0,
+            ticks: [0, .25, 0.5, 0.75, 1]
+        }
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("serviced-on-time"));
+    chart.draw(data, options);
+}
+
+// Serviced at Required Time
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart3);
+
+function drawChart3() {
+    var data = google.visualization.arrayToDataTable([
+        ['Category', 'Under', 'Ok', 'Over', {role: 'annotation'}],
+        ['Atlanta', 15, 80, 5, ''],
+        ['San Antonio', 10, 80, 10, ''],
+        ['Jackson', 5, 75, 20, ''],
+        ['Fife', 40, 55, 5, ''],
+        ['Ontario', 10, 50, 40, ''],
+        ['Salt Lake', 25, 70, 5, ''],
+        ['Hawaii', 0, 100, 0, ''],
+        ['Alaska', 5, 95, 0, ''],
+        ['ALL', 20, 70, 10, '']
+    ]);
+
+    var options = {
+        title: 'Time In Store (%)',
+        legend: {position: 'top', maxlines: 3},
+        bar: {groupWidth: '50%'},
+        isStacked: 'percent',
+        vAxis: {
+            minValue: 0,
+            ticks: [0, .25, 0.5, 0.75, 1]
+        }
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("time-in-store"));
+    chart.draw(data, options);
+}
+
+google.charts.load('current', {'packages': ['line']});
+google.charts.setOnLoadCallback(drawLineChart);
+
+function drawLineChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Week');
+    data.addColumn('number', 'Stores Not Serviced');
+    data.addColumn('number', 'Not Serviced On Time');
+    data.addColumn('number', 'Questionable Hours Total');
+    data.addColumn('number', 'Questionable Hours Under');
+
+    data.addRows([
+        [1, 19, 15, 10, 5],
+        [2, 16, 17, 9, 6],
+        [3, 18, 25, 6, 5],
+        [4, 20, 20, 8, 7],
+        [5, 13, 16, 7, 8],
+        [6, 15, 13, 6, 10],
+        [7, 12, 13, 6, 10],
+        [8, 10, 11, 5, 9],
+        [9, 13, 15, 6, 10],
+        [10, 14, 13, 5, 9],
+        [11, 15, 14, 5, 9],
+        [12, 14, 12, 6, 10],
+        [13, 14, 12, 5, 11],
+        [14, 15, 12, 5, 10]
+    ]);
+
+    var options = {
+        chart: {
+            title: 'Weekly Trending (%)',
+        }
+    };
+
+    var chart = new google.charts.Line(document.getElementById('chart-trending'));
+    chart.draw(data, google.charts.Line.convertOptions(options));
 }
